a2217d53d1192e86c374b669c677d3488ccb9486
Justin Zhu <justinzhu0913@gmail.com>
2025-06-20 19:49:44 -0700

    actually linking the database to the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,21 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://uh0nthrbtq.ufs.sh/f/7t6GinQRTocOUvU2SNnxDoAc6gV5juMm7QGWbTa2LUzKfneB",
-  "https://uh0nthrbtq.ufs.sh/f/7t6GinQRTocOegBdiFOfBAVLjy7JMuEK9t1lgUXkPmN4x6hz",
-  "https://uh0nthrbtq.ufs.sh/f/7t6GinQRTocOitylHfo2rMNp4PAjC1y8k6dVJYHxSKz0EtTI", 
-  "https://uh0nthrbtq.ufs.sh/f/7t6GinQRTocOzOZYABj0DZGvFNnht5e742om8gICOdwKuxRA"
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
-
 // This component only runs on the server. It does not contain any client-side
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
         {/* flex-wrap makes more than one row if needed, won't stop at one row only */}
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="flex w-48 flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
