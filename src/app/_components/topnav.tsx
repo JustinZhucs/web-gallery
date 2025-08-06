c1692e185c0dcd2bf1babb30ccc016d99ed824ac
Justin Zhu <justinzhu0913@gmail.com>
2025-08-06 16:24:29 -0700

    more cleanup on modal

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -3,13 +3,16 @@
 import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/nextjs";
 import { UploadButton } from "~/utils/uploadthing";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export function TopNav() {
     const router = useRouter();
 
     return (
       <nav className="flex w-full items-center justify-between border-b border-white p-4 text-xl font-semibold">
-        <div className="text-3xl">Gallery</div>
+        <Link href="/" className="text-3xl duration-400 hover:text-teal-100 cursor-pointer">
+          Gallery
+        </Link>
   
         <div className="flex flex-row">
             <SignedOut>
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -4,6 +4,19 @@ export default async function FullPageImageView( props: { id: number }) {
 
   const image = await getImage(props.id);
   return (
-    <img src={image.url} className="w-96"/>
+    <div className="flex w-full h-full min-w-0">
+        <div className="flex-shrink flex justify-center items-center">
+            <img src={image.url} className="flex-shrink object-contain"/>
+            {/* 
+            Use the <Image> component from next/image to display the image
+            would generate fake elements in order to try and take up the 
+            right amount of space, although it would be better to use the 
+            Image component from next/image
+            */}
+        </div>
+        <div className="w-48 flex-col border-1">
+            <div className="text-xl font-bold">{image.name}</div>
+        </div>
+    </div>
   );
 }
